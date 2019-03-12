7080f88af22a1cb4516baa5c12a41bfffa43b03b
Tretyak Andru <andruuu5482@gmail.com>
2019-03-12 17:23:59 +0200

    four commit

--- two_9.java
+++ two_9.java
@@ -1,4 +1,8 @@
-public class five_10 {
+
+
+
+public class two_9 
+ {
 
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
@@ -9,21 +13,37 @@ public class five_10 {
 			mas[i]=(int)(Math.random()*200-100);
 		}
 		for (int i = 0; i < n; i++) {
-		
+			System.out.print("  "+mas[i]);
 		}
 		int mas2[]=shiftarrey(mas, 4);
 		System.out.println();
 		for (int i = 0; i < mas2.length; i++) {
 			System.out.print("  "+mas2[i]);
 		}
-//asdasdasds
 	}
 	
 	public static int[] shiftarrey(int[] mas, int j){
 		int mas2[]=new int[ mas.length];
-	
+		
+		int k = 0;
+		
+		
+		for (int i = j; i < mas2.length ; i++) {
+			mas2[k] = mas[i];
+			k++;
+		}
+		
+		for (int i = 0; i<j; i++) {
+			mas2[k] = mas[i];
+			k++;
+		}
+		
+		
+		
+		
 		return mas2;
 	}
 }
 
 		
+
